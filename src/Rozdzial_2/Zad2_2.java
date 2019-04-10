222357af4f137a6a850e1f0ad0dbda506f0bada6
damian.lukaszewski <lukaszewsidamian@gmail.com>
2019-04-10 20:50:34 +0200

    Commit5

--- src/Rozdzial_2/Zad2_2.java
+++ src/Rozdzial_2/Zad2_2.java
@@ -1,15 +1,52 @@
 package Rozdzial_2;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-public class Zad2_2 {
-    public static void main(String[] args) {
+import static java.lang.Math.sqrt;
 
-        double a,b,c;
+public class Zad2_2 {
+    public static void main(String[] args) throws IOException {
+
+        double a,b,c,delta,x1,x2;
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Podaj 3 Liczby do funkcji kwadratowej");
+        System.out.println("Podaj Liczbe a");
+        a = Double.parseDouble(reader.readLine());
+        if (a==0) {
+            System.out.println("Niedozwolona wartość liczba a nie moze miec wartosci rownej 0 ");
+        }else {
+            System.out.println("Liczba a = " + a);
+            System.out.println("Podaj Liczbe b");
+            b = Double.parseDouble(reader.readLine());
+            System.out.println("Liczba b = " + b);
+            System.out.println("Podaj Liczbe c");
+            c = Double.parseDouble(reader.readLine());
+            System.out.println("Liczba c = " + c);
 
+            delta = (b * b) - 4 * a * c;
+
+            if (delta < 0) {
+                System.out.println("Brak miejsc Zerowych");
+            } else if (delta == 0) {
+                x1 = (-b) / (2 * a);
+                System.out.println("Mamy jedno miejsce zerowe ");
+                System.out.print("x1 = ");
+                System.out.printf("%3.2f",x1);
+            } else if (delta > 0) {
+                x1 = (-b) + sqrt(delta) / (2 * a);
+                x2 = (-b) - sqrt(delta) / (2 * a);
+                System.out.println("Mamy dwa miejsca zerowe ");
+                System.out.print("x1 = ");
+                System.out.printf("%3.2f", x1);
+                System.out.println();
+                System.out.println("Oraz");
+                System.out.print("x2 = ");
+                System.out.printf("%3.2f", x2);
+            }
+        }
     }
 }
---
+++ src/Rozdzial_2/Zad2_4.java
@@ -0,0 +1,28 @@
+package Rozdzial_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Zad2_4 {
+    public static void main(String[] args)throws IOException {
+        double a,b,c,x;
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Podaj Liczby do obliczenia wartosci x z rownania ax + b = c");
+        System.out.println("Podaj liczbe a ");
+        a =Double.parseDouble(reader.readLine());
+        if (a ==0){
+            System.out.println("Niedozwolona wartość liczba a nie moze byc rowna 0");
+        }else {
+            System.out.println("Podaj Liczbe b ");
+            b = Double.parseDouble(reader.readLine());
+            System.out.println("Podaj Liczbe c ");
+            c = Double.parseDouble(reader.readLine());
+            x = (c - b) / a;
+            System.out.println("Liczba x jest rowna ");
+            System.out.printf("%3.2f",x);
+        }
+    }
+}
