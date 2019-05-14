4671ef98151a6968cfdff4e715919362dbca7d96
damian.lukaszewski <lukaszewsidamian@gmail.com>
2019-05-14 19:48:38 +0200

    Commit9

---
+++ src/Rozdzial_4/Zad4_11.java
@@ -0,0 +1,47 @@
+package Rozdzial_4;
+
+public class Zad4_11 {
+    public static void main(String[] args) {
+
+        int n = 10, i , j;
+
+        int a[][] = new int[n][n];
+        int b[][] = new int[n][n];
+        int c[][] = new int[n][n];
+        System.out.println("Tablica A");
+        for (i=0;i<a.length;i++){
+            for (j=0;j<a.length;j++){
+                a[i][j] = 3;
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Tablica B");
+        for(i=0;i<b.length;i++){
+            for (j=0;j<b.length;j++){
+                b[i][j] = 2;
+                System.out.print(b[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Tablica C ktora zawiera iloraz liczb z tablicy A i B");
+        for(i=0;i<c.length;i++){
+            for (j=0;j<c.length;j++){
+
+                for (int k=0;k<c.length;k++){
+                    c[i][j] += a[i][k]*b[k][j];
+
+                }System.out.print(c[i][j] + " ");
+
+
+            }System.out.println();
+
+        }
+
+    }
+
+
+
+
+
+}
---
+++ src/Rozdzial_4/Zad4_12.java
@@ -0,0 +1,13 @@
+package Rozdzial_4;
+
+public class Zad4_12 {
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+
+}
