30c29a2b027e4795c0898ed56c1581f865ca19a2
damian.lukaszewski <lukaszewsidamian@gmail.com>
2019-05-14 16:03:26 +0200

    Commit7

--- src/Rozdzial_3/Zad3_21.java
+++ src/Rozdzial_3/Zad3_21.java
@@ -6,7 +6,18 @@ public class Zad3_21 {
         int n = 10;
         int kolumny,wiersze;
 
+        wiersze = 1;
 
+        while (wiersze <=n){
+            kolumny = 1;
+            while (kolumny<=n){
+                
+                System.out.print(wiersze * kolumny + "\t");
+                kolumny++;
+            }
+            wiersze++;
+            System.out.println();
+        }
 
     }
 }
