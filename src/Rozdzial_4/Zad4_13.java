bc3a7d7e6b42117f14e0eeffa480a2710f856278
damian.lukaszewski <lukaszewsidamian@gmail.com>
2019-05-15 17:54:43 +0200

    Commit10

---
+++ src/Rozdzial_4/Zad4_13.java
@@ -0,0 +1,24 @@
+package Rozdzial_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Zad4_13 {
+    public static void main(String[] args) throws IOException {
+
+        String imie,nazwisko;
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+
+        System.out.println("Program Mierzy dlugosc lancucha znakow imie i nazwisko");
+        System.out.println("Podaj Imie ");
+        imie = bufferedReader.readLine();
+        System.out.println("Podaj Nazwisko ");
+        nazwisko = bufferedReader.readLine();
+        System.out.println("Podane imie ma " + imie.length() + " znaki");
+        System.out.println("Natomaist podane nazwisko ma : " + nazwisko.length() + " znaków");
+    }
+}
---
+++ src/Rozdzial_4/Zad4_14.java
@@ -0,0 +1,26 @@
+package Rozdzial_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Zad4_14 {
+    public static void main(String[] args)throws IOException {
+
+
+        String lancuch1,lancuch2;
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+
+        System.out.println("Program laczy ze soba dwa podane lancuchy znakow ");
+        System.out.println("Podaj pierwszy ciag znakow ");
+        lancuch1 = bufferedReader.readLine();
+        System.out.println("Podaj Drugi ciag znakow ");
+        lancuch2 = bufferedReader.readLine();
+
+        System.out.println("Polaczone lancuchy znakow to " + lancuch1 + lancuch2);
+        System.out.println("Polaczone lancuchy znakow to " + lancuch2 + lancuch1);
+        System.out.println("odworcone polaczenie jest niezrozumiale znaczy to ze konkatencja tzn laczenie znakow nie jest przemienne");
+    }
+}
